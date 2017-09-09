d71fd770edb9bb9d39723a5221c3c1036ded9fd6
Poloz Alexey <polozhev@mail.ru>
2017-09-09 14:55:45 +0300

    All replace to work in format text (enters.py)

--- base.py
+++ base.py
@@ -10,11 +10,12 @@ for i in read(name='twits'):
 			k.append(1)
 		else:
 			k[word.index(j)]+=1
+#Сделать проверку не по количеству слов везде, а по алотности в одном примере
 
 print(len(k))
 i=0
 while i<len(k):
-	if k[i]<=4 : #or word[i] in ('',)
+	if k[i]<=4: #or word[i] in ('',)
 		del k[i]
 		del word[i]
 	else:
--- calc.py
+++ calc.py
@@ -4,11 +4,13 @@ x=numread('table', 1).T
 for i in range(len(x[0])):
 	for j in range(7):
 		x[j][i]=1
-x=x.T
+x=x[6:]
+print(x)
 
-y=numread('table', 1).T[0:7].T
+y=numread('table', 1).T[0].T #0
+print(y)
 
-re=np.linalg.inv(x.T.dot(x)).dot(x.T)
+re=np.linalg.inv(x.dot(x.T)).dot(x)
 w=re.dot(y)
 
 np.savetxt('data/'+mas[ii][0]+'-weights.csv', w, delimiter=',')
