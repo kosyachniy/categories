7200cfa779c2a19079897e7692698fce8f4d8c6d
Poloz Alexey <polozhev@mail.ru>
2017-09-10 13:17:54 +0300

    Worked many out

--- perceptron.py
+++ perceptron.py
@@ -1,37 +1,46 @@
 import numpy as np
 #import random
 
-act = lambda xe, we: sum([xe.dot(we) for i in range(len(xe))])
+allcolumn=7
+
+act = lambda xe, we: sum([xe[i] * we[i] for i in range(len(xe))])
 
 with open('data/text-table.csv', 'r') as f:
-	x = np.loadtxt(f, delimiter=',', skiprows=1)
-with open('data/text-table.csv', 'r') as f:
-	y = np.loadtxt(f, delimiter=',', skiprows=1).T[0:7].T #:7
-
+	x = np.loadtxt(f, delimiter=',', skiprows=1).T[allcolumn-1:].T
 for i in range(len(x)):
-	for j in range(7):
-		x[i][j] = 1
-#x = x.T[6:].T
-w = np.zeros((len(x[0]), 7)) #random
+	x[i][0] = 1
 
-print(x)
-print(y)
-print(w)
+def neiro(column):
+	print('Out №{}'.format(column))
 
-for iteration in range(27):
-	print('Iteration №{}'.format(iteration+1))
+	with open('data/text-table.csv', 'r') as f:
+		y = np.loadtxt(f, delimiter=',', skiprows=1).T[column].T
 
-	for i in range(len(x)):
-		#Сделать ограничение на числа (ноль, бесконечность)
-		#print(len(x), len(x[0]), len(w), len(w[0]))
-		error = (y[i] - act(x[i], w))
-		print(error)
+	w = [0 for j in range(len(x[0]))] #random
 
-		for j in range(len(x[i])):
-			print('Δw%d = %f' % (j, x[i][j] * error))
-			w[j] += x[i][j] * error
+	print(x)
+	print(y)
+	print(w)
 
-		print('-----')
+	for iteration in range(27):
+		print('Iteration №{}'.format(iteration+1))
 
-np.savetxt('data/text-weights.csv', w, delimiter=',')
-print(w)
+		for i in range(len(x)):
+			#Сделать ограничение на числа (ноль, бесконечность)
+			error = (y[i] - act(x[i], w))
+
+			for j in range(len(x[i])):
+				print('Δw%d = %f' % (j, x[i][j] * error))
+				w[j] += x[i][j] * error
+
+			print('-----')
+
+	return w
+
+ai=[]
+for i in range(allcolumn):
+	ai.append(neiro(i))
+ai=np.array(ai).T
+
+np.savetxt('data/text-weights.csv', ai, delimiter=',')
+print(ai)
