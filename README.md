85952b89654374e8c6c01fb0b8f54789c4022f61
Poloz Alexey <polozhev@mail.ru>
2017-09-10 12:32:50 +0300

    Many out

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 File -> Base -> Convert -> Calc -> ...
 
-'''
+```
 python3 file.py
 python3 base.py
 python3 convert.py
 python3 calc.py
-'''
+```
--- perceptron.py
+++ perceptron.py
@@ -1,19 +1,18 @@
 import numpy as np
 #import random
 
-act = lambda xe, we: sum([xe[i] * we[i] for i in range(len(xe))])
+act = lambda xe, we: sum([xe.dot(we) for i in range(len(xe))])
 
 with open('data/text-table.csv', 'r') as f:
-	x = np.loadtxt(f, delimiter=',', skiprows=1).T
+	x = np.loadtxt(f, delimiter=',', skiprows=1)
 with open('data/text-table.csv', 'r') as f:
-	y = np.loadtxt(f, delimiter=',', skiprows=1).T[0].T
+	y = np.loadtxt(f, delimiter=',', skiprows=1).T[0:7].T #:7
 
-for i in range(len(x[0])):
+for i in range(len(x)):
 	for j in range(7):
-		x[j][i] = 1
-x = x[6:]
-x = x.T
-w = [0 for j in range(len(x[0]))] #random
+		x[i][j] = 1
+#x = x.T[6:].T
+w = np.zeros((len(x[0]), 7)) #random
 
 print(x)
 print(y)
@@ -24,7 +23,9 @@ for iteration in range(27):
 
 	for i in range(len(x)):
 		#Сделать ограничение на числа (ноль, бесконечность)
+		#print(len(x), len(x[0]), len(w), len(w[0]))
 		error = (y[i] - act(x[i], w))
+		print(error)
 
 		for j in range(len(x[i])):
 			print('Δw%d = %f' % (j, x[i][j] * error))
