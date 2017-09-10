7252feae9be8b5f8acc7be0b368e529f4a1d446a
Poloz Alexey <polozhev@mail.ru>
2017-09-10 14:29:51 +0300

    Text density

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# categories
+# Классификатор текстов
+новостей?
 
-File -> Base -> Convert -> Calc -> ...
+File -> Base -> Convert -> Perceptron -> VK
 
 ```
 python3 file.py
 python3 base.py
 python3 convert.py
-python3 calc.py
+python3 perceptron.py
+python3 vk.py
 ```
+
+1. Сделана проверка на плотность внутри предложения (убрать если будут маленькие предложения и заменить на выделение важных слов с помощью ИИ)
--- base.py
+++ base.py
@@ -1,25 +1,35 @@
 from func import *
 
-word=[] #set()
+words=[] #set()
 k=[]
 for i in read(name='twits'):
+	word=[]
 	for j in i[7:]:
 		if j not in word:
-			word.append(j) #add(j)
+			word.append(j)
+
+		'''
+		 #add(j)
 #
 			k.append(1)
 		else:
 			k[word.index(j)]+=1
-#Сделать проверку не по количеству слов везде, а по алотности в одном примере
+		'''
 
+	#Проверка не по количеству слов везде, а по алотности в одном примере
+	words=sorted(set(word), key=word.count, reverse=True)[:10]
+
+'''
 print(len(k))
 i=0
 while i<len(k):
-	if k[i]<=4: #or word[i] in ('',)
+	if k[i]<=4 or word[i] in ('тот', 'какой', 'один', 'самый', 'год', 'быть', 'такой', 'который', 'фото'): #or word[i] in ('',)
 		del k[i]
 		del word[i]
 	else:
 		i+=1
 print(len(k))
-#
+#Убирать слишком частые слова и слишком редкие (?)
+'''
+
 write(word, name='base', typ='w')
