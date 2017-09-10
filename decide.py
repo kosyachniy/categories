80ed5a6a6a851caadd19212e9356f5252760bf8c
Poloz Alexey <polozhev@mail.ru>
2017-09-10 14:11:40 +0300

    All categories clasificator

--- decide.py
+++ decide.py
@@ -1,18 +1,41 @@
 from func import *
+import math
 
 def decide(cont):
 	word=read(name='base')[0]
-	w=numread('weights')
+	ww=numread('weights').T
 
-	k=0
-	su=w[0]
-	for i in text(cont):
-		if i in word:
-			k+=1
-			su+=w[word.index(i)+1]
+	cont=text(cont)
 
-	su=round(su, 2) if k else 0
-	return ('Наука ({}%)'.format(su*100) if su>=0.5 else 'Наука ({}%)'.format((1-su)*100)) if su else 'По этим данным невозможно определить категорию!'
+	result=[]
+	for j in range(len(ww)):
+		w=ww[j]
+		k=0
+		su=w[0]
+		for i in cont:
+			if i in word:
+				k+=1
+				su+=w[word.index(i)+1]
+
+		su=round(su, 2) if k else 0
+		result.append(su)
+
+	###
+	print(result)
+	for i in range(len(result)):
+		if math.isinf(result[i]):
+			result[i]=0
+	###
+
+	m=max(result) #!!![:6]
+	tex=''
+	for i in range(7): #6 Почему часто выбирает последнее?
+		if result[i]==m:
+			tex+=['Наука', 'Технологии', 'Новости', 'Публицистика', 'Диалог', 'Юмор', 'Информация'][i]+'\n' #'Наука ({}%)'.format(su*100) if su>=0.5 else 'Наука ({}%)'.format((1-su)*100)) if su else 'По этим данным невозможно определить категорию!'
+	#Убрать ответ, если сразу все категории
+	#Почему бесконечность?
+	#Убрать 0
+	return tex
 
 if __name__=='__main__':
 	a=decide(input())
