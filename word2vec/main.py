bca825cef0466f16d4e8cb16708e18e2ab57cb23
Poloz Alexey <polozhev@mail.ru>
2018-01-26 00:40:00 +0300

    Word2Vec Worked

--- word2vec/main.py
+++ word2vec/main.py
@@ -1,14 +1,25 @@
 from func import *
 
-categories = formed(('Технологии', 'Новости', 'Публицистика', 'Диалог', 'Юмор', 'Информация', 'Выражения', 'Наука'))
+categories = formed(('Технологии', 'Новости', 'Публицистика', 'Диалог', 'Юмор', 'Информация', 'Выражения', 'Наука', 'Рецепты', 'Происшествие', 'Событие', 'Афиша'))
 
-while True:
-	x = formed(input().split())
-	print(x)
-
+def vector(x):
 	try:
-		x = model.most_similar_to_given(x, categories)
+		y = model.most_similar_to_given(x, categories)
 	except:
-		print('Ошибка рассчёта!')
+		try:
+			x = model.most_similar_cosmul(positive=x)
+			print(x)
+			y = model.most_similar_to_given(x[0][0], categories)
+		except:
+			print('По этим данным невозможно определить категорию!')
+		else:
+			print(y.split('_')[0], model.distance(x[0][0], y))
 	else:
-		print(x.split('_')[0])
+		print(y.split('_')[0], model.distance(x, y))
+
+if __name__ == '__main__':
+	while True:
+		x = formed(input().split())
+		print(x)
+
+		print(vector(x))
